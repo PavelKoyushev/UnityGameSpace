71d052dbcf42f6e2aa98b643ad95e904d730bc3e
PavelKoyushev <pkoyushev@gmail.com>
2020-11-13 09:39:35 +0300

    Game
    First Game

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float tilt;
+    public float speed;
+    public float xMin, xMax, zMin, zMax;
+    Rigidbody ship;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ship = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        ship.velocity = new Vector3(moveHorizontal, 0, moveVertical) * 20;
+
+        float positionX = Mathf.Clamp(ship.position.x, xMin, xMax);
+        float positionZ = Mathf.Clamp(ship.position.z, zMin, zMax);
+        ship.position = new Vector3(positionX, 0, positionZ);
+
+        ship.rotation = Quaternion.Euler(tilt * ship.velocity.z, 0, -ship.velocity.x * tilt);
+    }
+}
